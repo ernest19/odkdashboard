8f69f309bf21fa21665528a0dfaec18662c8c4a0
ernest opoku-kwarteng <eopokukwarteng@gmail.com>
2022-10-26 21:58:25 +0000

    s

--- odkdashboard/settings.py
+++ odkdashboard/settings.py
@@ -79,16 +79,29 @@ WSGI_APPLICATION = 'odkdashboard.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'mydb',
+#         'USER': 'postgres',
+#         'PASSWORD':'ds@2022',
+#         'HOST':'52.54.23.108', 
+#         'PORT':'5432',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'mydb',
+        'NAME': 'surveydb',
         'USER': 'postgres',
-        'PASSWORD':'ds@2022',
-        'HOST':'52.54.23.108', 
+        'PASSWORD':'isd@2022!',
+        'HOST':'localhost', 
         'PORT':'5432',
     }
 }
+
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
